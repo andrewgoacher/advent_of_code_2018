37b91aac6ea2d8ef453b3048af6ef870afd0f762
Andrew Goacher <a.goacher@shieldsafety.co.uk>
2018-12-07 15:32:49 +0000

    Closer to actually compiling

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,33 +1,23 @@
 use util::get_lines;
 use std::collections::HashMap;
 
-struct CheckSum {
-    two: i32,
-    three: i32
-}
-
-fn get_checksum(input: &string) -> CheckSum {
-    let hm = input.chars()
-        .fold(HashMap::new(),
-         |mut m, c| { *m.entry(c).or_insert(0) += 1; m});
-    let mut two = 0;
-    let mut three = 0;
-
-    for (k,v) in hm {
-        if v == 2 {
-            two +=1;
-        } else if v == 3 {
-            three += 1;
-        }
-    }
-    CheckSum {two = two, three = three}
-}
-
-pub fn solve() -> i32 {
+pub fn solve() -> u32 {
     let lines = get_lines("./data/day_2.txt");
     let hashmap = HashMap::new();
     let checksums = lines   
                     .into_iter()
+                    .fold(hashmap,
+                    |mut m, l| {
+                        l.chars()
+                            .fold(m,
+                                |mut m2, c| {
+                                    *m2.entry(c).or_insert(0) += 1; m2
+                                });
+                                m
+                    });
+    
+    let twos_count = checksums.iter().filter(|&(k,v)| *v == 2).count() as u32;
+    let threes_count = checksums.iter().filter(|&(k,v)| *v == 3).count() as u32;
 
-    0
+    twos_count * threes_count
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod util;
 mod day_1;
+mod day_2;
 
 fn main() {
     let day_1_result = day_1::solve();
     println!("{}",day_1_result);
+
+    let day_2_result = day_2::solve();
+    println!("{}", day_2_result);
+
     ()
 }
