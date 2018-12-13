94aaff73f675686be53a99c5d77cd88783bda6ab
Andrew Goacher <andrewgoacher@outlook.com>
2018-12-13 23:32:10 +0000

    stuff

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ mod day_2;
 
 fn main() {
     let day_1_result = day_1::solve();
-    println!("{}",day_1_result);
+    println!("day 1 - part 1: {}",day_1_result);
 
     let day_1_part_2 = day_1::solve_part2();
-    println!("{}", day_1_part_2);
+    println!("day 1 - part 2: {}", day_1_part_2);
 
     let day_2_result = day_2::solve();
-    println!("{}", day_2_result);
+    println!("day 2 - part 1: {}", day_2_result);
 
     ()
 }
