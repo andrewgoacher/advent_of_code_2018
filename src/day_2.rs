7c4dcc80805d00d6dba8cfc64842caeb29ccc8d0
Andrew Goacher <a.goacher@shieldsafety.co.uk>
2018-12-18 13:18:26 +0000

    Part 2 underway for day 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -3,9 +3,15 @@ mod util;
 use std::collections::HashMap;
 use util::{get_lines,get_file_path};
 
-pub fn solve(path: &String) -> u32 {
+pub fn part_1(path: &String) -> u32 {
     let lines = get_lines(path);
     solve_internal(lines)
+}
+
+fn part_2(path: &String) -> String {
+    let lines = get_lines(path);
+
+    solve_part_2_internal(lines)
 }
 
 fn solve_internal(lines: Vec<String>) -> u32 {
@@ -31,11 +37,43 @@ fn solve_internal(lines: Vec<String>) -> u32 {
     twos * threes
 }
 
+fn solve_part_2_internal(lines: Vec<String>) -> String {
+    let ordered_lines : Vec<String> = lines.into_iter().map(|l| {
+       let mut chars : Vec<char> = l.chars().collect();
+       chars.sort_by(|a,b| a.cmp(b)) ;
+       let len = chars.len()-1;
+       let c : String = chars
+            .into_iter()
+            .enumerate()
+            .filter(|&(i,_)| i < len )
+            .map(|(_,ch)| ch)
+            .collect();
+       c
+    }).collect();
+
+    // todo: learn rust properly
+    println!("{:?}", ordered_lines);
+
+    let res : Vec<String> = ordered_lines.into_iter().fold(HashMap::new(), |mut acc, line| {
+        *acc.entry(line).or_insert(1) += 1;
+        acc
+    })
+    .iter()
+    .filter(|&(_,v)| *v > 1)
+    .map(|(k,_)| k.clone())
+    .collect();
+
+    res[0].clone()
+}
+
 fn main() {
     let file_path = get_file_path().unwrap();
 
-    let part_1 = solve(&file_path);
+    let part_1 = part_1(&file_path);
     println!("Part 1: {}", part_1);
+
+    let part_2 = part_2(&file_path);
+    println!("Part 2: {}", part_2);
 }
 
 #[cfg(test)]
@@ -55,5 +93,17 @@ mod tests {
         let result = solve_internal(strings);
 
         assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn part_2_solve() {
+        let strings = vec![
+            String::from("abcdef"),
+            String::from("abcdeg"),
+            String::from("avbsde")
+        ];
+
+        let result = solve_part_2_internal(strings);
+        assert_eq!(result, String::from("abcde"));
     }
 }
